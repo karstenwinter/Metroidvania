0f50e484cf3f585871bec8f302c62a80ee0351c2
Karsten <brox.p@web.de>
2020-06-27 19:44:23 +0200

    update

---
+++ Assets/Main.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Assets
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var s = "";
+		//	Texture2D.Load ("");
+			//Unity.Mathematics.math.
+			Console.WriteLine (s);
+		}
+	}
+}
